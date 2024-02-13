5f1b3c5b0e3177d8d85bed677dcc16c62766a78a
ali257715 <159857764+ali257715@users.noreply.github.com>
2024-02-12 19:55:35 -0500

    Delete ripped.guide directory

--- ripped.guide/js/dark.js
+++
@@ -1,3 +0,0 @@
-function switchTheme(){const currentIsDark=document.documentElement.classList.contains("dark");localStorage.setItem("theme",currentIsDark?"light":"dark");document.documentElement.classList.toggle("dark");}
-if(localStorage.getItem("theme")==="dark")
-document.documentElement.classList.add("dark");
